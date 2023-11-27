1b4e5d04e3ff9d822110797fab9efce2249265f1
Tomkov1c <tomkliner9@gmail.com>
2023-11-27 20:22:04 +0100

    Dashboard delno

---
+++ src/home.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en" data-theme="light">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nadzorna plosca</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" type="text/css" href="style/global.css"/>
+    <link rel="stylesheet" type="text/css" href="style/home.css"/>
+</head>
+<body>
+    <section id="sectionL">
+        <div class="sectionLButton">
+            <i class="fa-solid fa-house sectionLButtonIcon"></i>
+            <p class="sectionLButtonText">Domov</p>
+        </div>
+        <div class="sectionLButton">
+            <i class="fa-solid fa-list-ul sectionLButtonIcon"></i>
+            <p class="sectionLButtonText">Lestvica</p>
+        </div>
+        <div class="sectionLButton">
+            <i class="fa-solid fa-file-pen sectionLButtonIcon"></i>
+            <p class="sectionLButtonText">Tekmovanja</p>
+        </div>
+        <div class="sectionLButton">
+            <i class="fa-solid fa-graduation-cap sectionLButtonIcon"></i>
+            <p class="sectionLButtonText">Uspeh</p>
+        </div>
+    </section>
+    <section id="sectionR">
+        
+    </section>
+</body>
+</html>
+<script src="script/dash.js"></script>
