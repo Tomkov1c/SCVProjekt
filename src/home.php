387743dabad023d3ff9443c8f471310bc7b33c84
Pungi <131058916+pungiu@users.noreply.github.com>
2024-06-04 13:46:04 +0200

    nekaj

--- src/home.php
+++ src/home.php
@@ -8,54 +8,29 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Nadzorna Plošča</title>
 </head>
+<?php session_start(); ?>
 <body>
     <section id="sectionL">
-        <button id="active"><i class="fa-solid fa-house"></i><p>Dom</p></button>
-        <button><i class="fa-solid fa-hashtag"></i><p>Vnos</p></button>
-        <button><i class="fa-solid fa-eye"></i><p>Pogled</p></button>
-        <button><i class="fa-solid fa-chair"></i><p>3. TRB</p></button>
+        <button onclick="switchSub('subpages/home.html', this)" id="active"><i class="fa-solid fa-house"></i><p>Dom</p></button>
+        <button onclick="switchSub('subpages/vnos.html', this)"><i class="fa-solid fa-hashtag"></i><p>Vnos</p></button>
+        <button onclick="switchSub('subpages/pogled.html', this)"><i class="fa-solid fa-eye"></i><p>Pogled</p></button>
+        <button onclick="switchSub('subpages/razred.html', this)"><i class="fa-solid fa-compass-drafting"></i><p>3. TRB</p></button>
+        <button><i class="fa-solid fa-compass-drafting"></i><p>3. TRA</p></button>
+        <?php
+            if ($_SESSION['stat'] == true) {
+                ?>
+                <button onclick="switchSub('subpages/admin.html', this)"><i class="fa-solid fa-shield"></i><p>Admin</p></button>
+        <?php
+            }
+        ?>
+        <!--Bottom-->
+        <button onclick="switchSub('subpages/nastavitve.html', this)" class="bottom"><i class="fa-solid fa-gear"></i><p>Nastavitve</p></button>
+        <button><i class="fa-solid fa-right-from-bracket" ></i><a href="prijava.php?action=logout">Izpis</a></button>
     </section>
     <section id="sectionR">
-        <h1>UI Elements</h1>
-        <br>
-        <?php
-        session_start();
+        <iframe id="iframe" src="subpages/home.html" data-theme="dark" frameborder="0" sandbox="allow-same-origin allow-scripts">
 
-        ?>
-        <br>
-        <h1>Heading 1</h1>
-        <h2>Heading 2</h2>
-        <h3>Heading 3</h3>
-        <p>Adipisci ducimus accusamus saepe esse quas quae in libero cupiditate, est unde doloremque soluta, deserunt maiores eveniet dolorem labore explicabo laborum sint, quibusdam recusandae iure nemo rem sunt voluptatibus dolorem aspernatur quod?</p>
-        <ol>
-            <li>ipsum</li>
-            <li>quos</li>
-            <li>alias</li>
-            <li>laboriosam</li>
-        </ol>
-        <ul>
-            <li>ipsum</li>
-            <li>quos</li>
-            <li>alias</li>
-            <li>laboriosam</li>
-        </ul>
-
-
-
-        <div class="highlightRow">
-            <img src="images/testProfilePicture.jpeg" alt="Loading">
-            <p class="name"><?=
-                htmlspecialchars($_SESSION["uname"]); //v primeru da je iem zelo golgo se rabi text pomanast
-                ?></p>
-        </div>
-
-        <div class="submenuSplitSelect">
-            <div><p>2019/2020</p></div>
-            <div><p>2020/2021</p></div>
-            <div><p>2021/2022</p></div>
-            <div><p>2022/2023</p></div>
-            <div><p>2023/2024</p></div>
-        </div>
+        </iframe>
 
     </section>
 </body>
@@ -63,13 +38,15 @@
         <h1>Zavihki</h1>
         <p>Kako bi naj zgledali zavihki na tej strani. Vsak je .html datoteka. Način navigacije na zavihke je pa odvisen od tega za kaj se bomo odločili.</p>
         <div class="display_inline_flex">
-            <a href="samples/home.html"><i class="fa-solid fa-house"></i><p>Dom</p></a>
-            <a href="samples/vnos.html"><i class="fa-solid fa-hashtag"></i><p>Vnos</p></a>
-            <a href="samples/pogled.html"><i class="fa-solid fa-eye"></i><p>Pogled</p></a>
-            <a href="samples/"><i class="fa-solid fa-chair"></i><p>Razred</p></a>
-            <a href="samples/"><i class="fa-solid fa-chair"></i><p>Odjava</p></a> <!-- more it v drugo vrsto! -->
+            <a href="subpages/home.html"><i class="fa-solid fa-house"></i><p>Dom</p></a>
+            <a href="subpages/vnos.html"><i class="fa-solid fa-hashtag"></i><p>Vnos</p></a>
+            <a href="subpages/pogled.html"><i class="fa-solid fa-eye"></i><p>Pogled</p></a>
+            <a href="subpages/"><i class="fa-solid fa-chair"></i><p>Razred</p></a>
+            <a href="subpages/nastavitve.html"><i class="fa-solid fa-gear"></i><p>Nastavitce</p></a>
+            <?php var_dump($_SESSION); ?>
         </div>
     </div>
 <script src="script/darkMode.js"></script>
 <script src="script/devMenu.js"></script>
+<script src="script/subpageSwitch.js"></script>
 </html>
--- src/prijava.php
+++ src/prijava.php
@@ -1,120 +1,146 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Prijava</title>
-    <link rel="stylesheet" type="text/css" href="style/global.css" />
-    <link rel="stylesheet" type="text/css" href="style/login.css"/>
-</head>
-<!--
-<header>
-<h1 class="appName">Dosežki</h1>
-<h2 class="subAppName">Prijava</h2>
-</header>
--->
-<body>
 <?php
+require_once 'povezava.php';
+?>
+    <!DOCTYPE html>
+    <html lang="en" data-theme="light" id="darmModeID">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Prijava</title>
+        <link rel="stylesheet" type="text/css" href="style/global.css" />
+        <link rel="stylesheet" type="text/css" href="style/login.css"/>
+    </head>
+    <!--
+    <header>
+    <h1 class="appName">Dosežki</h1>
+    <h2 class="subAppName">Prijava</h2>
+    </header>
+    -->
+    <body>
+    <?php
     $appid = "f73324d4-392d-402d-a1b7-9b72268e29ee";
     $tennantid = "f6232921-d0d7-4c1a-9eee-0da15213004d";
     $login_url = "https://login.microsoftonline.com/" . $tennantid . "/oauth2/v2.0/authorize";
     session_start();
     session_regenerate_id();
     $_SESSION['state'] = session_id();
-?>
-<div class="theForm">
-    <div class="theForm-L">
-        <h1 id="prijavaText">Prijava</h1>
-        <h5 id="prijavaSubText">Šolski Center Velenje</h5>
-        <form action="" method="get">
-            <label for="emso">
-            <input type="password" name="emso" maxlength="13" class="inputEMSO" placeholder="EMŠO" autofocus>
-            <input type="submit" value="Prijava" class="inputPrijava">
-        </form>
-        <hr>
-        <?php
+    ?>
+
+    <section>
+        <div id="sideL">
+            <h1>Prijava</h1>
+            <h2>Dijak / Dijakinja</h2>
+            <form action="" method="get">
+                <input type="password" name="emso" maxlength="13" class="inputField" placeholder="EMŠO" autofocus>
+                <input type="submit" value="Prijava" class="inputPrijava">
+            </form>
+            <hr>
+
+            <?php
             if (isset($_SESSION['msatg'])) {
                 echo '<p><a href="?action=logout">Log Out</a></p>'; //to je samo za test ce hoces lahka naredis css za to, ni pa nujno itak noben ne bo vidu.
                 header('Location: http://localhost/home.php');
             } else {
-                ?>
-                <a href="?action=login" class="loginWithMicrosoft-Wrapper">
-                    <div class="loginWithMicrosoft">
-                        <img alt="Loading ..." src="images/Logos/Microsoft.svg" id="microsoftLogo">
-                        <p id="loginText">Microsoft račun</p>
-                    </div>
+            ?>
+                <a id="microsoftPrijava" href="?action=login">
+                    <img alt="Loading..." src="images/Logos/Microsoft.svg">
+                    <p>Microsoft prijava</p>
                 </a>
                 <?php
             }
-        ?>
-    </div>
-    <div class="theForm-R">
+            ?>
+        </div>
+        <div id="sideR">
 
+        </div>
+    </section>
+
+        <div class="theForm-R">
+
+        </div>
     </div>
-</div>
-</body>
-</html>
+    </body>
+    </html>
+    <script src="script/darkMode.js"></script>
 <?php
 if ($_GET['action'] == 'login'){
-   $params = array ('client_id' =>$appid,
-      'redirect_uri' =>'http://localhost/prijava.php',
-      'response_type' =>'token',
-       'response_mode' =>'form_post',
-      'scope' =>'https://graph.microsoft.com/User.Read',
-      'state' =>$_SESSION['state']);
-   header ('Location: '.$login_url.'?'.http_build_query ($params));
+    $params = array ('client_id' =>$appid,
+        'redirect_uri' =>'http://localhost/prijava.php',
+        'response_type' =>'token',
+        'response_mode' =>'form_post',
+        'scope' =>'https://graph.microsoft.com/User.Read',
+        'state' =>$_SESSION['state']);
+    header ('Location: '.$login_url.'?'.http_build_query ($params));
 }
 
 if (array_key_exists ('access_token', $_POST))
- {
-   $_SESSION['t'] = $_POST['access_token'];
-   $t = $_SESSION['t'];
-$ch = curl_init ();
-curl_setopt ($ch, CURLOPT_HTTPHEADER, array ('Authorization: Bearer '.$t,
-                                            'Conent-type: application/json'));
-curl_setopt ($ch, CURLOPT_URL, "https://graph.microsoft.com/v1.0/me/");
-curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
-$rez = json_decode (curl_exec ($ch), 1);
-if (array_key_exists ('error', $rez)){
- var_dump ($rez['error']);
- die();
-}
-else  {
-$_SESSION['msatg'] = 1;
-$_SESSION['uname'] = $rez["displayName"];
-$_SESSION['id'] = $rez["id"];
-
-if (isset($rez['id'])) {
-    $ca = curl_init ();
-    curl_setopt ($ca, CURLOPT_HTTPHEADER, array ('Authorization: Bearer '.$t,
+{
+    $_SESSION['t'] = $_POST['access_token'];
+    $t = $_SESSION['t'];
+    $ch = curl_init ();
+    curl_setopt ($ch, CURLOPT_HTTPHEADER, array ('Authorization: Bearer '.$t,
         'Conent-type: application/json'));
-    curl_setopt ($ca, CURLOPT_URL, "https://graph.microsoft.com/beta/users/" . $rez['id']);
-    curl_setopt ($ca, CURLOPT_RETURNTRANSFER, 1);
-    $resu = json_decode (curl_exec ($ca), 1);
+    curl_setopt ($ch, CURLOPT_URL, "https://graph.microsoft.com/v1.0/me/");
+    curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+    $rez = json_decode (curl_exec ($ch), 1);
+    if (array_key_exists ('error', $rez)){
+        var_dump ($rez['error']);
+        die();
+    }
+    else  {
+        $_SESSION['msatg'] = 1;
+        $_SESSION['uname'] = $rez["displayName"];
+        $_SESSION['id'] = $rez["id"];
 
-    if (isset($resu['onPremisesDistinguishedName'])) {
-        $statusprijave = $resu['onPremisesDistinguishedName'];
+        if (isset($rez['id'])) {
+            $ca = curl_init ();
+            curl_setopt ($ca, CURLOPT_HTTPHEADER, array ('Authorization: Bearer '.$t,
+                'Conent-type: application/json'));
+            curl_setopt ($ca, CURLOPT_URL, "https://graph.microsoft.com/beta/users/" . $rez['id']);
+            curl_setopt ($ca, CURLOPT_RETURNTRANSFER, 1);
+            $resu = json_decode (curl_exec ($ca), 1);
 
-        if (str_contains($statusprijave, 'Dijaki')){ // ??Mogoce kak boljsi nacin?? (Zaposleni)
-            $_SESSION['stat'] = true;
-        } else {
-            $_SESSION['stat'] = false;
+            if (isset($resu['onPremisesDistinguishedName'])) {
+                $statusprijave = $resu['onPremisesDistinguishedName'];
+
+                if (str_contains($statusprijave, 'Dijak')){ // ??Mogoce kak boljsi nacin?? (Zaposleni,Dijak)
+                    $_SESSION['stat'] = true;
+                } else {
+                    $_SESSION['stat'] = false;
+                }
+
+            } else {
+                $_SESSION['nekaj'] = false;
+            }
+
+            curl_close($ca);
         }
-
-    } else {
-        $_SESSION['nekaj'] = false;
     }
-
-    curl_close($ca);
-    }
-}
-curl_close ($ch);
-   header ('Location: http://localhost/prijava.php');
-   die();
+    curl_close ($ch);
+    header ('Location: http://localhost/prijava.php');
+    die();
 }
 if ($_GET['action'] == 'logout'){
     session_destroy();
     unset ($_SESSION);
     header ('Location: http://localhost/prijava.php');
     die();
+}
+
+if(isset($_POST['emso']))
+{
+    $_SESSION['emso'] = $_POST['emso'];
+    //check if emso exists in database
+    $query = "SELECT ime FROM dijaki WHERE emso = '".$_SESSION['emso']."'";
+    $result = mysqli_query($conn, $query);
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        $_SESSION['ime'] = $row['ime'];
+
+        header('Location: home.php');
+    }
+    else if ($_POST['emso'] == 999){
+        header("Location: admin.php");
+    } else {echo "Esihsdgfusdg";}
+
 }
