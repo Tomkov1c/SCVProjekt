94793a8db868249bf8e71ff5639ce8e297152300
Tom Kliner <83597418+Tomkov1c@users.noreply.github.com>
2024-06-14 09:10:21 +0200

    new setting

--- src/script/devMenu.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('keydown', (event) => {
-  if(event.ctrlKey && (event.key ==  "Q" || event.key ==  "q") ) {
-    if (document.getElementById("devMenu").style.display == "block") {
-        document.getElementById("devMenu").style.display = "none";
-    }else {
-        document.getElementById("devMenu").style.display = "block";
-    }
-  }
-}, false);
